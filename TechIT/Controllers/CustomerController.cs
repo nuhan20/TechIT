405a16c1d7e700fbfc862cd76709adebb146f86c
nuhan <nuhan@DESKTOP-QRD1ASP>
2023-07-23 12:59:42 +0600

    Add project files.

---
+++ TechIT/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechIT.Models;
+
+namespace TechIT.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Customer(string Name, string Email, int MobileNo, string Gender, string Address, int Cus_Id)
+        {
+            var db = new TechITEntities();
+            var name = (from n in db.Customer
+                        where n.Cus_Id == Cus_Id
+                        select n).SingleOrDefault();
+            ViewBag.name = name.Name;
+            ViewBag.email = Email;
+            ViewBag.mobileNo = MobileNo;
+            ViewBag.gender = Gender;
+            ViewBag.address = Address;
+           
+            if (Sort.Equals("All"))
+            {
+                var rq = (from r in db.Requests
+                          where r.employee_id == id
+                          select r).ToList();
+
+                return View(rq);
+            }
+            return View();
+        }
+        [HttpGet]
+
+        public ActionResult Customer(CustomerModel customer)
+        {
+            return View();
+        }
+    }
+}
---
+++ TechIT/Models/CustomerModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace TechIT.Models
+{
+    public class CustomerModel
+    {
+
+        [Required]
+        [StringLength(30, ErrorMessage = "Please do not enter values over 30 characters")]
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Your Email is not valid.")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(11, ErrorMessage = "Do not enter more then 11 digit")]
+        [MinLength(11, ErrorMessage = "Do not enter less then 11 digit")]
+        [Display(Name = "Mobile No")]
+        public int MobileNo { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Your Gender")]
+        public string Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Current Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public System.DateTime DOB { get; set; }
+
+        public int Cus_Id { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+
+   
+}
